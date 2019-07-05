c84b8cca3b1bb48c0ef34b6aa8f488ad64ffcfe5
blackplusy <happyming07@sina.com>
2019-07-05 10:19:55 +0800

    Add files via upload

---
+++ 输入.py
@@ -0,0 +1,8 @@
+#coding=utf-8
+#input()函数可以接受键盘传入字符类型数据，需要有一个变量来接受值
+#括号中可以提示信息
+#a=input()
+#print('a的值是%s'% a)
+passwd=int(input('请输入您的密码：'))
+print(type(passwd))
+print(passwd)
---
+++ 输出.py
@@ -0,0 +1,42 @@
+#coding=utf-8
+#字符集、翻译官
+#print()函数括号中可以放入需要输出到终端上的数据
+
+#1.直接输出
+print('heygor is handsome!!')
+print(123)
+
+#2.变量输出
+a=10
+#a是变量名，10变量的值
+print(a)
+b=9
+print(a+b)
+
+#3.函数输出
+#abs()   绝对值
+#len()   元素个数，长度
+print(abs(-2))
+a='i like 55kai'
+print(len(a))
+
+#4.格式化输出
+#  %s   格式化显示字符串
+#  %d   格式化显示数字
+print('heygor is no.1')
+print('dola is no.2')
+print('big bear is no.3')
+print('------------------------')
+
+print('%s is no.%d'%('heygor',1))
+print('%s is no.%d'%('dola',2))
+
+print('你的英语成绩为%d'% 100)
+#print('您的英语成绩是%d'% '100')
+
+
+
+
+
+
+
