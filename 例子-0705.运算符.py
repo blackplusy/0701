715bc11a5146c51dbef5a0260264113c143b8695
blackplusy <happyming07@sina.com>
2019-07-05 11:36:37 +0800

    Add files via upload

---
+++ 例子-0705.运算符.py
@@ -0,0 +1,40 @@
+#coding=utf-8
+#算数运算符
+print(10+3)
+print(10-3)
+print(10*3)
+print(11/3)
+print(10%3)
+print(11//3)
+print(10**3)
+print(10**-2)
+print(9**0.5)
+print('------------')
+#比较运算符
+print(1>2)
+print(1==1)
+print('a'!='b')
+print('------------')
+#逻辑运算符
+#真 and 假==假
+print(1>2 and 1==1)
+#真 or 假==真
+print(1>2 or 1==1)
+#非假==真
+print(not(1>2))
+#成员运算符
+a='1234'
+for i in a:
+    print(i)
+
+l=['kobe','james','hotsource']
+if 'kobe' in l:
+    print('kobe is here!!')
+
+if 'heygor' not in l:
+    print('heygor is not here!')
+
+
+
+    
+
